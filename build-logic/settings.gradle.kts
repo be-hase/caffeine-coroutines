fbeb10ca61677a92895af634eca905a08a9cf209
Ryosuke Hasebe <hsb.1014@gmail.com>
2025-09-29 18:51:44 +0900

    Add foojay plugin (#107)

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -10,4 +10,9 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "build-logic"
