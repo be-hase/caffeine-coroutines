35b586cf2fb842926c40e75374f0d1baea406e13
ryosuke-hasebe <ryosuke.hasebe@lycorp.co.jp>
2025-09-29 18:49:14 +0900

    Add foojay plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,11 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "caffeine-coroutines"
 
 include("caffeine-coroutines")
